2207bd5f21fcc6d97b12b26f8f7dc737f8072afa
DianaKuznetsova <dianakyznetsova@yandex.by>
2020-11-29 13:06:15 +0300

    Basic abstraction created

---
+++ IMEIGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab12
+{
+    class IMEIGenerator
+    {
+        private static IMEIGenerator instance = null;
+        private int counter=0;
+        private IMEIGenerator()
+        {
+
+        }
+
+        public static IMEIGenerator getInstance()
+        {
+            if(instance == null)
+            {
+                instance = new IMEIGenerator();
+            }
+            return instance;
+        }
+
+        public string GenerateNewIMEI()
+        {
+            counter++;
+            return counter.ToString();
+        }
+    }
+}
---
+++ Phone.cs
@@ -0,0 +1,18 @@
+﻿namespace Lab12
+{
+    class Phone
+    {
+        public string Manufacturer { get; }
+        public string Model { get; }
+
+        public string IMEI { get; } // id
+
+        public Phone(string IMEI, string model, string manufacturer)
+        {
+            this.IMEI = IMEI;
+            this.Model = model;
+            this.Manufacturer = manufacturer;
+        }
+
+    }
+}
