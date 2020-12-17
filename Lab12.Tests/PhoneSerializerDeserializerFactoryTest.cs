8bf840703b9f0cde4b1b8d79afb6ce8dc040ff49
DianaKuznetsova <dianakyznetsova@yandex.by>
2020-12-17 23:54:14 +0300

    PhoneSerializerDeserializerFactory test added

---
+++ Lab12.Tests/PhoneSerializerDeserializerFactoryTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Lab12.Tests
+{
+    public class PhoneSerializerDeserializerFactoryTest
+    {
+        [Fact]
+        public void TestTextSerializerDeserializer()
+        {
+            SerializerDeserializer serializer = new PhoneSerialzierDeserializerFactory().CreateSerializerDeserializerForFile("test.txt");
+            Assert.IsType<TextSerializerDeserializer>(serializer);
+        }
+
+        [Fact]
+        public void TestBinSerializerDeserializer()
+        {
+            SerializerDeserializer serializer = new PhoneSerialzierDeserializerFactory().CreateSerializerDeserializerForFile("test.bin");
+            Assert.IsType<BinarySerializerDeserializer>(serializer);
+        }
+
+        [Fact]
+        public void TestXmlSerializerDeserializer()
+        {
+            SerializerDeserializer serializer = new PhoneSerialzierDeserializerFactory().CreateSerializerDeserializerForFile("test.xml");
+            Assert.IsType<XmlSerializerDeserializer>(serializer);
+        }
+
+        [Fact]
+        public void TestUnsupportedFileExtension()
+        {
+            Assert.Throws<Exception>(() =>
+            {
+                new PhoneSerialzierDeserializerFactory().CreateSerializerDeserializerForFile("test.csv");
+            });
+        }
+    }
+}
